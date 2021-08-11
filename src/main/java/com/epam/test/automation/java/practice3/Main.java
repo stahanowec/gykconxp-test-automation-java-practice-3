ff44bf28193317c42c2bf5c1df1c3fcd32b24ce2
API Client <api-client@example.com>
2021-08-11 16:50:48 +0000

    Initial commit

---
+++ src/main/java/com/epam/test/automation/java/practice3/Main.java
@@ -0,0 +1,35 @@
+package com.epam.test.automation.java.practice3;
+
+public class Main {
+
+    /**
+     * <summary>
+     * Implement code according to description of task 1.
+     * </summary>
+     */
+    public static int[] task1(int[] array) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task 2.
+     * </summary>
+     */
+    public static int task2(int[] array) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task 3.
+     * </summary>
+     */
+    public static int[][] task3(int[][] matrix) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+}
