92a8e6faa02645d0ece5a29c197b1948dc29b58f
Hurin Serhiy <sgurin@ukr.net>
2021-08-15 18:05:49 +0300

    Task 3. Final Implementation

--- src/main/java/com/epam/test/automation/java/practice3/Main.java
+++ src/main/java/com/epam/test/automation/java/practice3/Main.java
@@ -1,35 +1,100 @@
 package com.epam.test.automation.java.practice3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-    /**
-     * <summary>
-     * Implement code according to description of task 1.
-     * </summary>
-     */
+    private Main() {
+    }
+
     public static int[] task1(int[] array) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if (array.length > 2) {
+            for (var i = 0; i < array.length / 2; i++) {
+                var num = array[i];
+                var lastNum = array[array.length - i - 1];
+                if (isPrime(num, lastNum)) {
+                    array[i] = lastNum;
+                    array[array.length - i - 1] = num;
+                }
+            }
+            return array;
+        }
+        throw new IllegalArgumentException();
     }
 
-    /**
-     * <summary>
-     * Implement code according to description of task 2.
-     * </summary>
-     */
+    private static boolean isPrime(int secondNum, int thirdNum) {
+        return secondNum % 2 == 0 && thirdNum % 2 == 0;
+    }
+
     public static int task2(int[] array) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if (array.length > 1) {
+            Map<Integer, Pair> positions = new HashMap<>();
+            for (var i = 0; i < array.length; i++) {
+                var element = array[i];
+                var lists = positions.get(element);
+                if (lists == null) {
+                    positions.put(element, Pair.of(i));
+
+                } else {
+                    lists.updateLast(i);
+                }
+            }
+
+            var maxValue = positions.keySet().stream().max(Integer::compareTo)
+                    .orElseThrow(IllegalArgumentException::new);
+            var integerList = positions.get(maxValue);
+            if (integerList.isNotPresentLast()) {
+                return 0;
+            }
+            return integerList.calculate();
+        }
+        return 1;
     }
 
-    /**
-     * <summary>
-     * Implement code according to description of task 3.
-     * </summary>
-     */
     public static int[][] task3(int[][] matrix) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if (matrix.length > 0) {
+            var newMatrix = matrix.clone();
+            for (var i = 0; i < matrix.length; i++) {
+                for (var j = 0; j < matrix[i].length; j++) {
+                    if (i == j) {
+                        newMatrix[i][j] = matrix[i][j];
+                    } else if (i < j) {
+                        newMatrix[i][j] = 1;
+                    } else {
+                        newMatrix[i][j] = 0;
+                    }
+                }
+            }
+            return newMatrix;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static class Pair {
+        private final Integer first;
+        private Integer last;
+
+        private Pair(Integer first) {
+            this.first = first;
+        }
+
+        public static Pair of(Integer first) {
+            return new Pair(first);
+        }
+
+        public Pair updateLast(Integer last) {
+            this.last = last;
+            return this;
+        }
+
+        public boolean isNotPresentLast() {
+            return last == null;
+        }
+
+        public int calculate() {
+            return last - first;
+        }
     }
 
 }
--- src/test/java/com/epam/test/automation/java/practice3/MainTest.java
+++ src/test/java/com/epam/test/automation/java/practice3/MainTest.java
@@ -1,5 +1,66 @@
 package com.epam.test.automation.java.practice3;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.testng.Assert.assertEquals;
+
 public class MainTest {
 
+    @Test(dataProvider = "task1DataProvider")
+    public void ifTask1ThenSuccess(List<Integer> currentValue, List<Integer> expectedValue) {
+        //given
+        var current = currentValue.stream().mapToInt(i -> i).toArray();
+        var expected = expectedValue.stream().mapToInt(i -> i).toArray();
+        //when
+        var result = Main.task1(current);
+        //then
+        assertEquals(result, expected);
+    }
+
+    @Test(dataProvider = "task2DataProvider")
+    public void ifTask2ThenSuccess(List<Integer> currentValue, int expectedValue) {
+        //given
+        var current = currentValue.stream().mapToInt(i -> i).toArray();
+        var expected = expectedValue;
+        //when
+        var result = Main.task2(current);
+        //then
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void ifTask3ThenSuccess() {
+        //given
+        var current = new int[][] {{2, 4, 3, 3},{5, 7, 8, 5},{2, 4, 3, 3},{5, 7, 8, 5}};
+        var expected = new int[][] {{2, 1, 1, 1},{0, 7, 1, 1},{0, 0, 3, 1},{0, 0, 0, 5}};
+        //when
+        var result = Main.task3(current);
+        //then
+        assertEquals(result, expected);
+    }
+
+    @DataProvider(name = "task1DataProvider")
+    public static Object[][] task1DataProvider() {
+        return new Object[][]{
+                {List.of(10, 5, 3, 4), List.of(4, 5, 3, 10)},
+                {List.of(100, 2, 3, 4, 5), List.of(100, 4, 3, 2, 5)},
+                {List.of(100, 2, 3, 45, 33, 8, 4, 54), List.of(54, 4, 3, 45, 33, 8, 2, 100)},
+                {List.of(100, 2, 4, 45, 33, 8, 4, 54), List.of(54, 4, 8, 45, 33, 4, 2, 100)}};
+    }
+
+
+    @DataProvider(name = "task2DataProvider")
+    public static Object[][] task2DataProvider() {
+        return new Object[][]{
+                {List.of(4, 100, 3, 4), 0},
+                {List.of(5, 50, 50, 4, 5), 1},
+                {List.of(5, 350, 350, 4, 350), 3},
+                {List.of(10, 10, 10, 10, 10), 4}
+              };
+    }
 }
